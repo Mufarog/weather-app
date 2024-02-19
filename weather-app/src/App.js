3a376298b4afec009eade3bb7e10904b082fe2e8
Mnelisi <swangs.m09@gmail.com>
2024-02-19 23:22:45 +0200

    merging from main branch

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,65 @@
-import React from "react";
-import Home from './home'
+import React, {useState} from "react"
+import axios from "axios";
+
 
 function App() {
-    return (
-        <div>
-            <Home/>
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+ const url ='https://api.openweathermap.org/data/2.5/weather?q=london&units=imperial&appid=f67a0d4b7622e1d844dc76f0f9f7be95';  // API URL to get weather data
+ 
+
+ const searchLocation = (event)  =>{
+if (event.key === 'Enter') {
+  axios.get(url).then((response) => {
+    setData(response.data)
+    console.log(response.data);
+  })
+  setLocation('')
+}
+}
+
+  return (
+    <div className="App">
+      <div className="search">
+        <input
+        value={location}
+        onChange={event =>  setLocation(event.target.value)}
+        onKeyPress = {searchLocation}
+         placeholder="Search for a location..." 
+         type='text'/>
+      
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="Location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+           {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null }
+          </div>
         </div>
-    )
+        {data.name != undefined &&
+        <div className="bottom">
+          <div className="feel">
+            {data.main ? <p>{data.main.feels_like.toFixed()}°C</p> : null}
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+            <p>Humidity</p>
+          </div>
+        <div className="Wind">
+          {data.wind ? <p className="bold">{data.wind.speed.toFixed()}KM/H</p> :null}
+          <p>Wind Speed</p>
+        </div>
+      </div>
+      }
+    </div>
+    </div>
+  );
 }
 
 export default App
